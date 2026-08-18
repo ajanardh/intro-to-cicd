74a2216d1d5f2b8d32bace37eb7fd4476a783a2f
ajanardh <aswin.jan@gmail.com>
2026-08-17 22:06:46 -0400

    Add comment

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ function sayHi(name) {
 }
 
 module.exports = sayHi
+// Here is a comment to explain the code above. The function sayHi takes a name as an argument and returns a greeting string that includes the provided name. The function is then exported using module.exports so that it can be used in other files.
